dc33e47d44ec9f560573331caba625bb5492a903
Josemedvel <josemedvel@gmail.com>
2023-11-10 21:07:00 +0100

    Challenge_01 completed

--- src/challenge_01/mod.rs
+++ src/challenge_01/mod.rs
@@ -1,6 +1,35 @@
 use std::fs;
-pub fn challenge_01() {
-    let contents = fs::read_to_string("src/challenge_01/input_01.txt")
-        .expect("Failed to read").to_lowercase();
-    println!("{}",contents);
+use std::collections::HashMap;
+pub fn challenge_01(message:Option<String>) {
+    let contents;
+    match message{
+        Some(message) => {
+            contents = message.clone().to_lowercase();
+        },
+        None => {
+            contents = fs::read_to_string("src/challenge_01/input_01.txt")
+                .expect("Failed to read").to_lowercase();
+        }
+    }
+    let word_array:Vec<&str> = contents.split_whitespace().collect();
+    let mut word_order:Vec<&str> = Vec::new();
+    let mut map:HashMap<&str, i32> = HashMap::new();
+    for word in word_array{
+        let old_value = map.get(word).cloned();
+        match old_value {
+            Some(value) => {
+                map.insert(word, value + 1);
+            },
+            None => {
+                map.insert(word, 1);
+                word_order.push(word);
+            }
+        }
+    }
+    let mut result = String::new();
+    for word in word_order{
+        //result = result + word + map.get(word).expect("Error getting the value for the word").to_string().as_str(); //works
+        result.push_str(&format!("{}{}", word, map.get(word).expect("Error getting the value for the word")));
+    }
+    println!("{}",result);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod challenge_01;
 use challenge_01::challenge_01;
 fn main() {
-    challenge_01();
+    challenge_01(Some("llaveS casa CASA casa llaves".to_string()));
+    challenge_01(Some("taza ta za taza".to_string()));
+    challenge_01(Some("casas casa casasas".to_string()));
+    challenge_01(None);
 }
