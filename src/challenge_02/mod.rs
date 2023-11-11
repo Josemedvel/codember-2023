035c673ec52ac5bdfc2234d924b56b998ad60ea3
Josemedvel <josemedvel@gmail.com>
2023-11-11 21:33:19 +0100

    Challenge 02 completed

---
+++ src/challenge_02/mod.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+pub fn challenge_02(message:Option<String>) {
+    let contents;
+    match message{
+        Some(message) => {
+            contents = message.clone().to_lowercase();
+        },
+        None => {
+            contents = fs::read_to_string("src/challenge_02/input_02.txt")
+                .expect("Failed to read").to_lowercase();
+        }
+    }
+    let mut acc:i32 = 0;
+    //println!("{}", contents);
+    let op_list:Vec<&str> = contents.split("").filter(|&s| s != "").collect();
+    for x in op_list{
+        match x{
+            "#" => acc += 1,
+            "@" => acc -= 1,
+            "*" => acc = acc.pow(2),
+            "&" => print!("{:?}", acc),
+            _ => eprintln!("Error parsing message.")
+        }
+    }
+    println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-mod challenge_01;
-use challenge_01::challenge_01;
+//mod challenge_01;
+mod challenge_02;
+mod challenge_03;
+mod challenge_04;
+
+//use challenge_01::challenge_01;
+use challenge_02::challenge_02;
+
 fn main() {
-    challenge_01(Some("llaveS casa CASA casa llaves".to_string()));
-    challenge_01(Some("taza ta za taza".to_string()));
-    challenge_01(Some("casas casa casasas".to_string()));
-    challenge_01(None);
+    //challenge_01(Some("llaveS casa CASA casa llaves".to_string()));
+    //challenge_01(Some("taza ta za taza".to_string()));
+    //challenge_01(Some("casas casa casasas".to_string()));
+    //challenge_01(None);
+    //challenge_02(Some("##*&".to_string()));
+    //challenge_02(Some("&##&*&@&".to_string()));
+    challenge_02(None);
+    
 }
